e1c457de2cdbbef3e817ef03b4c222d574f97487
kerchnoah <kerchnoah@gmail.com>
2026-09-08 13:36:08 +0200

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/edu/psu/campusride/CampusRideApplication.java
@@ -0,0 +1,13 @@
+package edu.psu.campusride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusRideApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/psu/campusride/CampusRideApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.psu.campusride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
